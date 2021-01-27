8ce2ea0d55b274ce1e613ee62af57ca5891c5f38
Nick <62834545+MikeC0xl0ng@users.noreply.github.com>
2021-01-27 10:43:10 +0100

    Adding ThreadsSons

---
+++ ThreadsSons/src/threadssons/ThreadsSons.java
@@ -0,0 +1,11 @@
+package threadssons;
+
+// @author Bodliev
+
+public class ThreadsSons {
+
+    public static void main(String[] args) {
+        new ThreadsSonsClass();
+    }
+    
+}
---
+++ ThreadsSons/src/threadssons/ThreadsSonsClass.java
@@ -0,0 +1,32 @@
+package threadssons;
+
+ //@author Bodliev
+ 
+public class ThreadsSonsClass implements Runnable{
+    
+    ThreadsSonsClass(){
+        Thread primoT = Thread.currentThread();
+        System.out.println("Primo thread: " + primoT);
+        Thread nuovoT = new Thread(this, "Thread figlio");
+        System.out.println("\t\tThread figlio: " + nuovoT);
+        nuovoT.start();
+        try{
+            Thread.sleep(2000);
+        }catch (InterruptedException e){
+            System.out.println("Primo thread: Mi hanno interrotto");
+        }
+        System.out.println("Primo thread terminato");
+    }
+    
+    public void run(){
+        try{
+            for (int i = 1; i < 5; i++) {
+                System.out.println("\t\t" + i);
+                Thread.sleep(1000);
+            }
+        }catch (InterruptedException e){
+            System.out.println("\t\tThread figlio: Mi hanno interrotto");
+        }
+        System.out.println("\t\tThread figlio terminato");
+    }
+}
